5efb71e1372cb57750e101da080c8157ee5c015a
Jonathan Bruckman <jbruckman86@gmail.com>
2022-09-23 21:19:08 -0400

    commit tree af4c9a07d76bdfdc94a3bafe08a1dafc1610174c

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+// let num1 = 31
+// let num2 = 2
 
-// Write your code here
+// const multiply = num1 * num2;
+
+// const random = Math.round(Math.random() * 10) + 1;
+
+// let num3 = 12
+// let num4 = 3
+
+// const mod = num3 / num4;
+
+// const max = Math.max(2, 4, 10, 20);
+
+
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 100);
+
+const num3 = 12
+const num4 = 3
+
+const mod = num3 / num4
+
+const max = Math.max(1, 2, 20)
